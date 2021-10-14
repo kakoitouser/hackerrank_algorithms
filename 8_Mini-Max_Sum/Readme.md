1b33c7464497056ad6361fd7a74dea8a040cf99f
kakoitouser <rashitdinalizmhanov@gmail.com>
2021-10-14 05:38:52 -0700

    added value for the 8 task

---
+++ 8_Mini-Max_Sum/Readme.md
@@ -0,0 +1,5 @@
+# Mini-Max Sum
+
+[![Screenshot-1.png](https://i.postimg.cc/RZC5fJcC/Screenshot-1.png)](https://postimg.cc/9rknHM8s)
+___
+[![Screenshot-2.png](https://i.postimg.cc/762pD3y9/Screenshot-2.png)](https://postimg.cc/7GqBnThJ)
---
+++ 8_Mini-Max_Sum/app.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+func miniMaxSum(arr []int32) {
+	sum := int64(0)
+
+	max, min := int64(arr[0]), int64(arr[0])
+	for _, value := range arr {
+		if int64(value) > max {
+			max = int64(value)
+		}
+		if min > int64(value) {
+			min = int64(value)
+		}
+		sum += int64(value)
+	}
+	fmt.Println(sum-max, sum-min)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
